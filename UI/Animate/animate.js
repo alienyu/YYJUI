78b3114f12123b89d612d66d187bf91737c97802
alienyu <yuyongjian@pinganfang.com>
2016-01-30 16:25:47 +0800

    DG

--- UI/Animate/animate.js
+++ UI/Animate/animate.js
@@ -2,12 +2,11 @@ $ = YYJUI.baseAPI.$;
 Base = YYJUI.baseAPI;
 YYJUI.AnimateQueue = {}
 YYJUI:config = {
-	queueNum:0;
+	queueNum:0
 }
 YYJUI.Animate = function(ops) {
-	this.init();
-};
-YYJUI.AnimateBase = function() {
+	this.ops = {};
+	Base.extend(this.ops, ops);
 	this.init();
 };
 YYJUI.Animate.prototype = {
@@ -29,10 +28,28 @@ YYJUI.Animate.prototype = {
 			}
 			return isExist;
 		}
-		checkAnimateQueue(this.element) && (YYJUI.AnimateQueue = this.name = this.element);
+		!checkAnimateQueue(this.ops.element) && (YYJUI.AnimateQueue[this.ops.element] = []);
 		this.initAnimate();
 	},
 	initAnimate: function() {
+		var that = this;
+		function checkAttr(attr) {
+			Base.each(YYJUI.AnimateQueue[that.ops.element], function(eachAttr, i) {
+				if(eachAttr.attr == attr) {
+					return eachAttr;
+				} else {
+					return false;
+				}
+			})
+		}
+		Base.each(that.ops.attributes,function(data, index) {
+			if(checkAttr[data]) {
+				YYJUI.AnimateQueue[that.ops.element].push(Base.extend(checkAttr[data],Base.filterJSON(that.ops, ["element","attributes"])));
+			} else {
+				YYJUI.AnimateQueue[that.ops.element].push(Base.extend({attr:data}, Base.filterJSON(that.ops, ["element","attributes"])));
+			}
+		})
 
+		console.log(YYJUI.AnimateQueue);
 	}
 }
