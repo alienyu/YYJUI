c37dd4fda6f90b234b71d48a17b97f0e324382a3
alienyu <alienyu>
2016-02-02 14:16:35 +0800

    Dg

--- DEMO/animate/animate.js
+++ DEMO/animate/animate.js
@@ -3,13 +3,32 @@ window.onload = function() {
 		config: {},
 		init: function() {
 			this.newAnimate();
-			console.log(animate);
+			this.bindEvent();
 		},
 		newAnimate: function() {
-			animate = new YYJUI.Animate({
+			fuc.animate1 = new YYJUI.Animate({
 				element: "#caseOne",
 				attributes: ['height', 'width'],
-				duration: 2000
+				duration: 500,
+				callback: function() {
+					alert("让你点就点真乖！！");
+				}
+			});
+			fuc.animate2 = new YYJUI.Animate({
+				element: "#caseTwo",
+				attributes: ['height', 'width', 'left', 'top'],
+				duration: 500,
+				callback: function() {
+					alert("让你点就点真乖！！");
+				}
+			});
+		},
+		bindEvent: function() {
+			$("#caseOneBtn")[0].addEventListener("click", function(e) {
+				fuc.animate1.start(10).stop(300);
+			});
+			$("#caseTwoBtn")[0].addEventListener("click", function(e) {
+				fuc.animate2.stop(100);
 			})
 		}
 	}
